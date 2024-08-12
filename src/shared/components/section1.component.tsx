aae8b1f8e0c686e4b33648668dc1e789a2907c5d
Vanderson Erlan <vandersonerlan@Vandersons-MBP.bbrouter>
2024-08-12 20:05:42 -0300

    feat: section one

---
+++ src/shared/components/section1.component.tsx
@@ -0,0 +1,26 @@
+export const Section1 = () => {
+  return (
+    <section className="w-full min-h-[100dvh] bg-slate-400 flex flex-col lg:justify-center lg:items-center lg:flex-row gap-10 p-10">
+      <div className="border-[1px] border-slate-950 w-full lg:w-1/2 p-6">
+        <img
+          src="/estilo-de-vida-da-pessoa-que-decora-a-porta-da-frente.jpg"
+          alt="hotel"
+          className="w-full"
+          loading="lazy"
+        />
+      </div>
+
+      <article className="flex flex-col items-center gap-4 lg:w-1/2">
+        <h2 className="whisper text-7xl w-full text-center text-slate-950">
+          Hotel Ágata
+        </h2>
+
+        <p className="lora-thin md:w-1/2 text-center">
+          Localizado em Catú/BA, o hotel Ágata é uma ótima escolha para
+          viajantes. Se encontra no Bairro Pioneiro, está na entrada e perto de
+          tudo na cidade.
+        </p>
+      </article>
+    </section>
+  );
+};
