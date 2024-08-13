c86c4595df5ef03bd1277d41df57c5810a888a59
Vanderson Erlan <vandersonerlan@Vandersons-MBP.bbrouter>
2024-08-13 16:29:04 -0300

    feat: room list. style: room layout

--- src/shared/components/section2.component.tsx
+++ src/shared/components/section2.component.tsx
@@ -1,17 +1,56 @@
 // section para mostrar images dos quartos
+import { RoomCard } from "./room-card.component";
 export const Section2 = () => {
   return (
     <section
       className="w-full min-h-[100dvh] flex flex-col gap-4 p-10"
       id="quartos"
     >
-      <h1 className="courier-prime-regular text-6xl md:text-8xl lg:text-9xl text-slate-400">
+      <h1
+        data-aos="fade-left"
+        data-aos-delay="300"
+        className="courier-prime-regular text-6xl md:text-8xl lg:text-9xl text-slate-400"
+      >
         HOTEL ÁGATA
       </h1>
 
-      <p className="lora-thin">
+      <p data-aos="fade-left" data-aos-delay="670" className="lora-thin">
         Veja nossos quartos e escolha o que seja melhor para sua estadia.
       </p>
+
+      <ul className="w-full flex flex-col gap-4 justify-center items-center lg:grid lg:grid-cols-2">
+        <li className="w-full grid place-items-center">
+          <RoomCard
+            title="Quarto Simples"
+            description="Quarto com banheiro privado, ar condicionado e uma cama de solteiro."
+            image="/quarto-simples.jpg"
+          />
+        </li>
+
+        <li className="w-full grid place-items-center">
+          <RoomCard
+            title="Quarto Duplo"
+            description="Quarto com banheiro privado, ar condicionado e duas camas de solteiro."
+            image="/quarto-duplo.jpg"
+          />
+        </li>
+
+        <li className="w-full grid place-items-center">
+          <RoomCard
+            title="Suite Standart"
+            description="Quarto com banheiro privado, ar condicionado, uma cama de casal, mesa retrátil e armário."
+            image="/quarto-triplo.jpg"
+          />
+        </li>
+
+        <li className="w-full grid place-items-center">
+          <RoomCard
+            title="Suite Premium"
+            description="Quarto com banheiro privado, ar condicionado, uma cama king, mesa de escritório, varanda e armário."
+            image="/quarto-quadruplo.jpg"
+          />
+        </li>
+      </ul>
     </section>
   );
 };
