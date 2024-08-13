31988c2e2b93d5228ce3c7f712bc265ce1803633
Vanderson Erlan <vandersonerlan@Vandersons-MBP.bbrouter>
2024-08-13 10:32:55 -0300

    feat: section2

---
+++ src/shared/components/section2.component.tsx
@@ -0,0 +1,17 @@
+// section para mostrar images dos quartos
+export const Section2 = () => {
+  return (
+    <section
+      className="w-full min-h-[100dvh] flex flex-col gap-4 p-10"
+      id="quartos"
+    >
+      <h1 className="courier-prime-regular text-6xl md:text-8xl lg:text-9xl text-slate-400">
+        HOTEL ÁGATA
+      </h1>
+
+      <p className="lora-thin">
+        Veja nossos quartos e escolha o que seja melhor para sua estadia.
+      </p>
+    </section>
+  );
+};
