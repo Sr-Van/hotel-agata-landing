90e4e62ee18f7cb0be199aa7eaa8136213dc9967
Vanderson Erlan <vandersonerlan@Vandersons-MBP.bbrouter>
2024-08-15 14:38:27 -0300

    fix: putting id for section

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import { Home } from "./shared/components/home.component";
 import { MobileBar } from "./shared/components/mobileBar.component";
 import { Section1 } from "./shared/components/section1.component";
 import { Section2 } from "./shared/components/section2.component";
+import { Section3 } from "./shared/components/section3.component";
 
 import AOS from "aos";
 import "aos/dist/aos.css";
@@ -21,6 +22,7 @@ function App() {
       <Home />
       <Section1 />
       <Section2 />
+      <Section3 />
       <MobileBar />
     </div>
   );
--- src/shared/components/section3.component.tsx
+++ src/shared/components/section3.component.tsx
@@ -1,6 +1,9 @@
 export const Section3 = () => {
   return (
-    <section className="w-full flex flex-col lg:flex-row lg:justify-between lg:items-center p-10 gap-4 overflow-x-hidden bg-slate-400">
+    <section
+      id="restaurante"
+      className="w-full flex flex-col lg:flex-row lg:justify-between lg:items-center p-10 gap-4 overflow-x-hidden bg-slate-400"
+    >
       <article className="w-full h-full lg:w-1/2 flex flex-col items-center justify-center gap-4">
         <h1
           className="whisper w-full text-center text-4xl md:text-6xl"
